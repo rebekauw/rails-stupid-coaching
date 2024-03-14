b11756d32cd1cddcb77040f5de1386768de37188
Rebekah Wu <rebekah.wu4566@gmail.com>
2024-03-14 19:50:58 +1100

    complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
     def ask
-
     end
 
     def answer
-        @question = 
-        @answer =
+        if params[:question].include? "I am going to work right now!"
+            @couch_answer = "Great!"
+          elsif params[:question].include? "?"
+            @couch_answer = "Silly question, get dressed and go to work!"
+          elsif params[:question].include? "!"
+            @couch_answer = "I don't care, get dressed and go to work!"
+          end
     end
 end
